cc0f1ca6cd30a235614fd66f8e01c5ae854a4d06
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-06 11:04:44 +0300

    Backend: register models with the admin site

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Topic, Room, Message, Review
 
 # Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Review)
