e6c889957a16ded257fececb8c3890b7a804ee31
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-06 15:34:53 +0300

    Backend: configure cors-headers

--- backend/settings.py
+++ backend/settings.py
@@ -38,12 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'base.apps.BaseConfig',
-
     'rest_framework',
+    'corsheaders',
+
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from 'react-bootstrap'
+import { listRooms } from '../actions/roomActions'
 
 function HomeScreen() {
+
+    const roomList = useSelector(state => state.roomList)
+    const {loading, error, rooms} = roomList
+    console.log(rooms)
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(listRooms())
+    }, [dispatch])
+
   return (
     <div>
       <h3>STUDY ROOMS</h3>
