b091596a63d23b4756c2c638bcaf28194c631825
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-06 11:25:37 +0300

    Backend: create the getrooms and getroom views and url

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name='get-routes'),
     path('rooms/', views.getRooms, name='get-rooms'),
+    path('rooms/<str:pk>/', views.getRoom, name='get-room'),
 ]
