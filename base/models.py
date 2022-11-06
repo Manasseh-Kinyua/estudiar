fc2abbc5e5c72e343256bc48a01780f21155e0e4
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-06 10:53:55 +0300

    Backend: create the Review table

--- base/models.py
+++ base/models.py
@@ -33,3 +33,14 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body[0:20]
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=250, null=True, blank=True)
+    rating = models.IntegerField(default=0, null=True, blank=True)
+    comment = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.rating)
