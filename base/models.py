f7ef4d6a4313709298908d9e1304dac856729819
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-06 10:46:48 +0300

    Backend: create the Room table

--- base/models.py
+++ base/models.py
@@ -9,3 +9,17 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.name
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=250)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    numReviews = models.IntegerField(default=0, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
