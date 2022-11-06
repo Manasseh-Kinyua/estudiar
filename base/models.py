00514f65d52e3b4132bdb445a53a3c523348a84d
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-06 10:39:32 +0300

    Backend: create the Topic table

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
