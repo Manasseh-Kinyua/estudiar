cc99aa9d9ce46d0644505831775a6f95030fe450
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-06 10:49:39 +0300

    Backend: create the Message table

--- base/models.py
+++ base/models.py
@@ -23,3 +23,13 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:20]
