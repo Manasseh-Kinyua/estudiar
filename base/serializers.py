5a1c006ce1713ddfad81d09af00d9ebfd1a71e2d
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-06 11:16:16 +0300

    Backend: nest the message and review serializers into the room serializer

--- base/serializers.py
+++ base/serializers.py
@@ -6,7 +6,30 @@ class TopicSerializer(serializers.ModelSerializer):
         model = Topic
         fields = '__all__'
 
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
 class RoomSerializer(serializers.ModelSerializer):
+    messages = serializers.SerializerMethodField(read_only=True)
+    reviews = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Room
         fields = '__all__'
+
+    def get_messages(self, obj):
+        messages = obj.message_set.all()
+        serializer = MessageSerializer(messages, many=True)
+        return serializer.data
+
+    def get_reviews(self, obj):
+        reviews = obj.review_set.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return serializer.data
--- base/views.py
+++ base/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Room
+from .serializers import RoomSerializer
 
 # Create your views here.
 
@@ -15,9 +17,6 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getRooms(request):
-    rooms = [
-        'room1',
-        'room2',
-        'room3',
-    ]
-    return Response(rooms)
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
