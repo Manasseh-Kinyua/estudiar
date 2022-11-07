5e4e2c78aa49dcb2640d4402e6713fa2c9613e66
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-07 11:25:55 +0300

    Frontend: dispatch list rooms action

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -3,13 +3,14 @@ import { Card, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Avatar from '@mui/material/Avatar';
 import Chip from '@mui/material/Chip';
+import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
 import Rating from './Rating';
 
 function Room({room}) {
   return (
       <Card style={{backgroundColor: 'rgb(1, 15, 32)', }}>
         <Card.Body>
-          <Row className='py-2'>
+          <Row className='pt-2 pb-4'>
             <Col style={{display: 'flex',justifyContent: 'spaceBetween', alignItems: 'center', color: '#46B5D1'}}>
               <Avatar alt="Remy Sharp" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFNkJpg5tIB3DZsMqxgGCyRtSwDuav9LEdbZI06evMasI6tmkPahgI1ftvuA7qbHSsbgg&usqp=CAU" />
               <small style={{marginLeft: '4px'}}>@{room.host.username}</small>
@@ -19,7 +20,6 @@ function Room({room}) {
               <Link to={`/room/${room.id}`} style={{marginLeft: '.5rem'}}><i className="fa-solid fa-ellipsis-vertical"></i></Link>
             </Col>
           </Row>
-          <hr style={{height: '1px', backgroundColor: 'white', color: 'white'}} />
           <Link to={`room/${room.id}`}>
             <Card.Title>
               {room.name}
@@ -34,6 +34,9 @@ function Room({room}) {
             </div>
         </Card.Text>
         <hr style={{height: '1px', backgroundColor: 'white', color: 'white'}} />
+        <Card.Text as='div'>
+            <PeopleOutlineIcon /> {room.participants.length} joined
+        </Card.Text>
         </Card.Body>
       </Card>
   )
