844b74147b4f46c1ec0d91fcfe534a8b4d4ecb7e
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-08 10:21:52 +0300

    Backend: customize token settings

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+
 urlpatterns = [
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name='get-routes'),
     path('rooms/', views.getRooms, name='get-rooms'),
     path('rooms/<str:pk>/', views.getRoom, name='get-room'),
