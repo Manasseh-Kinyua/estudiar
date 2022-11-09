254986398adca60c97b709639e4a1f4f18c0400e
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-09 19:36:48 +0300

    Frontend: create the feed action

--- frontend/src/actions/roomActions.js
+++ frontend/src/actions/roomActions.js
@@ -6,8 +6,12 @@ import {
     ROOM_DETAIL_REQUEST,
     ROOM_DETAIL_SUCCESS,
     ROOM_DETAIL_FAIL,
+
+    MESSAGE_LIST_REQUEST,
+    MESSAGE_LIST_SUCCESS,
+    MESSAGE_LIST_FAIL,
 } from "../constants/roomConstants";
-import { GET_ALL_ROOMS_ENDPOINT } from "../constants/apiConstants";
+import { GET_ALL_MESSAGES_ENDPOINT, GET_ALL_ROOMS_ENDPOINT } from "../constants/apiConstants";
 import { GET_SINGLE_ROOM_ENDPOINT } from "../constants/apiConstants";
 import axios from 'axios'
 
@@ -48,6 +52,28 @@ export const listRoomDetails = (id) => async (dispatch) => {
     }catch(error) {
         dispatch({
             type: ROOM_DETAIL_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+} 
+
+export const listAllMessages = () => async (dispatch) => {
+    try {
+        dispatch({
+            type: MESSAGE_LIST_REQUEST
+        })
+
+        const {data} = await axios.get(GET_ALL_MESSAGES_ENDPOINT)
+        dispatch({
+            type: MESSAGE_LIST_SUCCESS,
+            payload: data
+        })
+
+    }catch(error) {
+        dispatch({
+            type: MESSAGE_LIST_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message
