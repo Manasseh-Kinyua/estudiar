1073bb0c41931af5ce045d557c7b8b94aede25b7
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-11-10 16:46:59 +0300

    Frontend: create the topic list action

--- frontend/src/actions/roomActions.js
+++ frontend/src/actions/roomActions.js
@@ -10,8 +10,12 @@ import {
     MESSAGE_LIST_REQUEST,
     MESSAGE_LIST_SUCCESS,
     MESSAGE_LIST_FAIL,
+
+    TOPIC_LIST_REQUEST,
+    TOPIC_LIST_SUCCESS,
+    TOPIC_LIST_FAIL,
 } from "../constants/roomConstants";
-import { GET_ALL_MESSAGES_ENDPOINT, GET_ALL_ROOMS_ENDPOINT } from "../constants/apiConstants";
+import { GET_ALL_MESSAGES_ENDPOINT, GET_ALL_ROOMS_ENDPOINT, GET_ALL_TOPICS_ENDPOINT } from "../constants/apiConstants";
 import { GET_SINGLE_ROOM_ENDPOINT } from "../constants/apiConstants";
 import axios from 'axios'
 
@@ -74,6 +78,28 @@ export const listAllMessages = () => async (dispatch) => {
     }catch(error) {
         dispatch({
             type: MESSAGE_LIST_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message
+        })
+    }
+} 
+
+export const listAllTopics = () => async (dispatch) => {
+    try {
+        dispatch({
+            type: TOPIC_LIST_REQUEST
+        })
+
+        const {data} = await axios.get(GET_ALL_TOPICS_ENDPOINT)
+        dispatch({
+            type: TOPIC_LIST_SUCCESS,
+            payload: data
+        })
+
+    }catch(error) {
+        dispatch({
+            type: TOPIC_LIST_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message
