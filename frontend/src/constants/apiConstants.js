59667607bd4e499038793f151b4941a29170628c
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-30 11:02:18 +0300

    change to https

--- frontend/src/constants/apiConstants.js
+++ frontend/src/constants/apiConstants.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://127.0.0.1:8000/api/"
-export const BASE_URL = "http://estudiar.up.railway.app/api/"
+export const BASE_URL = "https://estudiar.up.railway.app/api/"
 
 export const GET_ALL_ROOMS_ENDPOINT = `${BASE_URL}rooms/`
 export const GET_SINGLE_ROOM_ENDPOINT = `${BASE_URL}rooms/`
