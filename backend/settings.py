c1b4114a1eaab252170df5e1788b004db03097a4
Manasseh Kinyua <95216131+Manasseh-Kinyua@users.noreply.github.com>
2022-12-30 10:39:50 +0300

    fix secret key

--- backend/settings.py
+++ backend/settings.py
@@ -26,11 +26,11 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-!&-yuuo)+jj8^qhbxooac7wkf=yse20$zvo!@p99luoq)3f=f$'
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-!&-yuuo)+jj8^qhbxooac7wkf=yse20$zvo!@p99luoq)3f=f$'
+# SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'nasseh-estudiar.herokuapp.com', 'web-production-2294.up.railway.app']
 
